b05e4232a978cac52f065859aca8837f1602eaf8
ddussi <tmqdyfud1@gmail.com>
2023-11-06 22:37:37 +0900

    💄 Design: common_css 추가

---
+++ client/src/styles/common/common_css.ts
@@ -0,0 +1,12 @@
+interface CommonCss {
+  [key: string]: string;
+}
+
+export const COMMON_CSS: CommonCss = {
+  "main-color": "#EFF1F3",
+  "sub-color": "#4F709C",
+  "point-color": "#213555",
+  "warning-color": "#ED4337",
+  "font-color-gray": "#70757A",
+  "font-color-black": "#414141",
+};
