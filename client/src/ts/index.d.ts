224791d61009f7951ce7bbd549fc0fa911f24e4d
Jess-Apr <jessie1944@naver.com>
2023-11-07 20:26:37 +0900

    🎸 Chore: index.d.ts 파일 생성 후 정적 파일 타입 선언

--- client/src/ts/index.d.ts
+++ client/src/ts/index.d.ts
@@ -2,3 +2,9 @@ declare module "*.css" {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
