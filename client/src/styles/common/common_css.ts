059c8c9da8b9eca6f1530c03ddf4e510d2369e80
Jess-Apr <jessie1944@naver.com>
2023-11-09 21:17:30 +0900

    ✏️ Style: common_css.ts의 warning-color 변경

--- client/src/styles/common/common_css.ts
+++ client/src/styles/common/common_css.ts
@@ -6,7 +6,7 @@ export const COMMON_CSS: CommonCss = {
   "main-color": "#EFF1F3",
   "sub-color": "#4F709C",
   "point-color": "#213555",
-  "warning-color": "#ED4337",
+  "warning-color": "#E43636",
   "font-color-gray": "#70757A",
   "font-color-black": "#414141",
 };
