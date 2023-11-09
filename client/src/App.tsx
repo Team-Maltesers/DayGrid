048c4f5e89d74ff8b273851c3963773d0bd8eb1e
ddussi <tmqdyfud1@gmail.com>
2023-11-09 23:31:24 +0900

    ✨ Feat: 라우팅 구조 추가 및 웹팩 서버 설정 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,41 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
 import React from "react";
+import Info from "./pages/Info";
+import Calendar from "./pages/Calendar";
+import DiaryWrite from "./pages/DiaryWrite";
+import DiaryList from "./pages/DiaryList";
+import MyPage from "./pages/MyPage";
+import { queryClient } from "./utils/http";
+import RootLayout from "./pages/Root";
+import Error from "./pages/Error";
 
-const App = (): JSX.Element => {
-  return <h1>Hello world!</h1>;
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Navigate to="/info" replace /> },
+      { path: "/info", element: <Info /> },
+      {
+        path: "/calendar",
+        element: <Calendar />,
+      },
+      { path: "/diary-write", element: <DiaryWrite /> },
+      { path: "/diary-list", element: <DiaryList /> },
+      { path: "my-page", element: <MyPage /> },
+      { path: "*", element: <Navigate to="/404" replace /> },
+      { path: "/404", element: <Error /> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+}
 
 export default App;
