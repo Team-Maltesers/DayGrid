3a569d52ebf154c4c1f6dd7148c0abe2186c4475
Jess-Apr <jessie1944@naver.com>
2023-11-10 16:23:57 +0900

    ✨ Feat: 햄버거 메뉴 클릭 시 메뉴 오픈

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleMenuModal, menuModalState } from "../../store/modal/menuModalSlice";
+import MenuModal from "./MenuModal";
 import classes from "../../styles/common/Header.module.css";
 import logoList from "../../assets/header/logo";
 import logoName from "../../assets/header/logo-name.svg";
@@ -6,27 +9,41 @@ import hamburger from "../../assets/header/hamburger.svg";
 
 const Header = (): JSX.Element => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const dispatch = useDispatch();
+  const menuState = useSelector(menuModalState);
+
+  useEffect(() => {
+    setIsModalOpen(menuState);
+  }, [menuState]);
 
   return (
-    <div className={classes.header__bg}>
-      <div className={classes.header__empty}></div>
-      <div className={classes.header__logo_con}>
-        <div className={classes.header__logo}>{logoList[0].logo()}</div>
-        <div className={classes.header__logo_name}>
-          <img src={logoName} />
+    <>
+      {isModalOpen ? <MenuModal /> : null}
+      <div className={classes.header__bg}>
+        <div className={classes.header__empty}></div>
+        <div className={classes.header__logo_con}>
+          <div className={classes.header__logo}>{logoList[0].logo()}</div>
+          <div className={classes.header__logo_name}>
+            <img src={logoName} />
+          </div>
         </div>
+        {isLoggedIn ? (
+          <div className={classes.header__menu_con}>
+            <img
+              src={hamburger}
+              className={classes.header__menu}
+              onClick={() => dispatch(toggleMenuModal())}
+            />
+          </div>
+        ) : (
+          <div className={classes.header__btn_con}>
+            <button className={classes.header__btn_reverse}>로그인</button>
+            <button>회원가입</button>
+          </div>
+        )}
       </div>
-      {isLoggedIn ? (
-        <div className={classes.header__menu_con}>
-          <img src={hamburger} className={classes.header__menu} />
-        </div>
-      ) : (
-        <div className={classes.header__btn_con}>
-          <button className={classes.header__btn_reverse}>로그인</button>
-          <button>회원가입</button>
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
