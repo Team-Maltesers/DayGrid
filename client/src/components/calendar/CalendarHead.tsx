6c177eb6a4b96658b84b0a68929de12773c6dca9
Jess-Apr <jessie1944@naver.com>
2023-11-13 21:19:31 +0900

    ✨ Feat: 메인 달력 헤더 화살표 클릭 시 년도, 월 변경 기능 구현

--- client/src/components/calendar/CalendarHead.tsx
+++ client/src/components/calendar/CalendarHead.tsx
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
+import { subMonths, addMonths } from "date-fns";
 import classes from "../../styles/calendar/Calendar.module.css";
 import leftArrow from "../../assets/image/arrow-left.png";
 import rightArrow from "../../assets/image/arrow-right.png";
 
 const CalendarHead = (): JSX.Element => {
-  const [currentYear, setCurrentYear] = useState<number>(new Date().getFullYear());
-  const [currentMonth, setCurrentMonth] = useState<number>(new Date().getMonth() + 1);
+  const [currentMonth, setCurrentMonth] = useState<Date>(new Date());
+
+  function moveToPrevMonth() {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  }
+
+  function moveToNextMonth() {
+    console.log(currentMonth);
+    setCurrentMonth(addMonths(currentMonth, 1));
+  }
 
   return (
     <div className={classes.calendar__head_con}>
       <div className={classes.calendar__head_left}>
         <div>
-          <img src={leftArrow} />
-          <img src={rightArrow} />
+          <img src={leftArrow} onClick={moveToPrevMonth} />
+          <img src={rightArrow} onClick={moveToNextMonth} />
         </div>
         <div>
-          {currentYear}년 {currentMonth}월
+          {currentMonth.getFullYear()}년 {currentMonth.getMonth() + 1}월
         </div>
       </div>
       <div className={classes.calendar__head_right}>
