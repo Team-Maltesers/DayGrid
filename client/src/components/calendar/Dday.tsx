5a9cacd981148166ce6bf76ccf5fb9f069e258fb
Jess-Apr <jessie1944@naver.com>
2023-11-14 21:01:09 +0900

    ✨ Feat: D-day 리스트 기능 구현

---
+++ client/src/components/calendar/Dday.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import DdayCard from "./DdayCard";
+import classes from "../../styles/calendar/Dday.module.css";
+
+const Dday = (): JSX.Element => {
+  return (
+    <div className={classes.dday__con}>
+      <div className={classes.dday__title}>D - DAY</div>
+      <div className={classes.dday__contents}>
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+        <DdayCard />
+      </div>
+    </div>
+  );
+};
+
+export default Dday;
