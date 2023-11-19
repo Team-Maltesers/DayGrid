c1ec77677815a09869b119f319e1db6de9b655ce
ddussi <114420277+ddussi@users.noreply.github.com>
2023-11-19 16:26:37 +0900

    ✨ Feat: 다이어리 ID를 관리하기 위한 Redux Slice 추가

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "./modal/modalSlice";
 import menuModalReducer from "./modal/menuModalSlice";
+import diaryReducer from "./diary/diarySlice";
 
 const store = configureStore({
   reducer: {
     modal: modalReducer, // 스토어에 모달 리듀서를 추가합니다.
     menuModal: menuModalReducer,
+    diaryId: diaryReducer,
   },
 });
 
