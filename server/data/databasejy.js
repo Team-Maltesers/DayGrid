9afeb0959a869c1844d43436778c8b9076e4948d
Jess-Apr <jessie1944@naver.com>
2023-11-28 14:15:21 +0900

    🎸 Chore: node_module 폴더 위치 변경, 환경변수 파일 생성

--- server/data/databasejy.js
+++ server/data/databasejy.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
   host: "localhost",
   database: "daygrid",
-  user: "root",
-  password: "1234qwer",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
