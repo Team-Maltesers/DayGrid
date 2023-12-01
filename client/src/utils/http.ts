87b3b4369928093da9f0d47b4f2bb806dc71c48b
ddussi <114420277+ddussi@users.noreply.github.com>
2023-12-01 16:09:10 +0900

    ♻️ Refactor: 로그인 상태 및 토큰을 Redux store에서 가져오도록 변경

--- client/src/utils/http.ts
+++ client/src/utils/http.ts
@@ -3,7 +3,7 @@ import { FormData } from "../components/SignUpForm";
 import { LoginFormData } from "../components/LoginForm";
 import { PostPlanData } from "../ts/PlanData";
 import axios from "axios";
-
+import store from "../store/store";
 export const queryClient = new QueryClient();
 
 const instance = axios.create({
@@ -12,16 +12,39 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
-    const token = "your token";
+    const token = store.getState().auth.accessToken;
     if (token) {
       config.headers["Authorization"] = "Bearer " + token;
     }
     return config;
   },
-  (error) => {
+  async function (error) {
+    /*
+    const originalRequest = error.config;
+
+    // 1. Unauthorized 상태이고, 이전에 토큰 갱신 요청이 아니었을 경우
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true; // 토큰 갱신 요청 플래그를 true로 설정
+
+      // 2. 토큰 갱신 요청
+      const access_token = await refreshAccessToken();
+
+      // 3. 새로운 토큰을 headers에 설정
+      axios.defaults.headers.common["Authorization"] = "Bearer " + access_token;
+
+      // 4. 이전에 실패했던 요청을 다시 보내기
+      return instance(originalRequest);
+    }
+*/
     return Promise.reject(error);
   },
 );
+/*
+async function refreshAccessToken() {
+  // 여기에 리프레시 토큰을 이용하여 새로운 엑세스 토큰을 발급받는 API 요청 로직을 작성하세요.
+  // 발급받은 엑세스 토큰을 반환해야 합니다.
+}
+*/
 
 export async function signup(eventData: FormData) {
   try {
@@ -234,17 +257,17 @@ export async function editUserInfo({
   try {
     let response;
     if (name) {
-      response = await instance.patch(`http://localhost:3000/my-page`, {
+      response = await instance.patch(`/my-page`, {
         id: id,
         name: name,
       });
     } else if (password) {
-      response = await instance.patch(`http://localhost:3000/my-page`, {
+      response = await instance.patch(`/my-page`, {
         id: id,
         password: password,
       });
     } else if (birthday) {
-      response = await instance.patch(`http://localhost:3000/my-page`, {
+      response = await instance.patch(`/my-page`, {
         id: id,
         birthday: birthday,
       });
@@ -263,7 +286,7 @@ export async function deleteUserInfo({ id }: { id: number | undefined }) {
     throw new Error("id is required");
   }
   try {
-    const response = await instance.delete(`http://localhost:3000/my-page`, { params: { id: id } });
+    const response = await instance.delete(`/my-page`, { params: { id: id } });
     return response.data;
   } catch (error) {
     throw error;
